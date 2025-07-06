e17b66001be95d75fa4d49fb873d480832d4517e
hardikr23 <hardikr68@gmail.com>
2025-07-07 00:44:58 +0100

    added code to deploy on cloud run. it is working

---
+++ microservices/backend/cr_deployment.sh
@@ -0,0 +1,22 @@
+# Set your Google Cloud Project ID
+export GOOGLE_CLOUD_PROJECT="gen-lang-client-0353594207"
+
+# Set your desired Google Cloud Location
+export GOOGLE_CLOUD_LOCATION="europe-west2" # Example location
+
+# Set the path to your agent code directory
+export AGENT_PATH="./src" # Assuming capital_agent is in the current directory
+
+# Set a name for your Cloud Run service (optional)
+export SERVICE_NAME="sports-doctor"
+
+# Set an application name (optional)
+export APP_NAME="sports-doctor-app"
+
+adk deploy cloud_run \
+--project=$GOOGLE_CLOUD_PROJECT \
+--region=$GOOGLE_CLOUD_LOCATION \
+--service_name=$SERVICE_NAME \
+--app_name=$APP_NAME \
+--with_ui \
+$AGENT_PATH
